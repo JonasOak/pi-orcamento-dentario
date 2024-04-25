e045f1056d8d9e60dae57d31bc441cf75acca6c9
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-04-25 18:05:57 -0300

    exceção personalizada de conexão

---
+++ DentalSystem/src/main/java/db/DbException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package db;
+
+public class DbException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public DbException(String msg) {
+        super(msg);
+    }
+    
+}
