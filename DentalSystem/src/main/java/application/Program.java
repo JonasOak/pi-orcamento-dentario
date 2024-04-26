443cdb42aa2718253e4767480aa123d4f51fb082
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-04-25 22:59:54 -0300

    teste leitura de dados

--- DentalSystem/src/main/java/application/Program.java
+++ DentalSystem/src/main/java/application/Program.java
@@ -6,14 +6,43 @@ package application;
 
 import db.DB;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     
     public static void main(String[] args) {
         
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
         
-        DB.closeConnection();
+        try {
+            conn = DB.getConnection();
+            
+            st = conn.createStatement();
+            
+            rs = st.executeQuery("select * from Cliente");
+            
+            while (rs.next()) {
+                System.out.println(rs.getInt("id_cliente") + ", " 
+                                + rs.getString("nome") + ", "
+                                + rs.getString("endereco") + ", "
+                                + rs.getString("uf") + ", "
+                                + rs.getString("telefone") + ", "
+                                + rs.getString("documento") + ", "
+                                + rs.getString("email"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
         
     }
     
