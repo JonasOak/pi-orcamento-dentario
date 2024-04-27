97945e14450b997c84bb771e97287f087add0087
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-04-27 18:37:14 -0300

    testando insert

--- DentalSystem/src/main/java/application/Program.java
+++ DentalSystem/src/main/java/application/Program.java
@@ -6,44 +6,41 @@ package application;
 
 import db.DB;
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Program {
     
     public static void main(String[] args) {
         
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-        
+        PreparedStatement st = null;
         try {
             conn = DB.getConnection();
             
-            st = conn.createStatement();
+            st = conn.prepareStatement(
+                    "INSERT INTO usuario"
+                    + "(nome, cargo, login, senha, email)"
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)");
             
-            rs = st.executeQuery("select * from Cliente");
+            st.setString(1, "Yuri");
+            st.setString(2, "Administrador");
+            st.setString(3, "yuripaixao");
+            st.setString(4, "12345678");
+            st.setString(5, "yuripaixao@gmail.com");
             
-            while (rs.next()) {
-                System.out.println(rs.getInt("id_cliente") + ", " 
-                                + rs.getString("nome") + ", "
-                                + rs.getString("endereco") + ", "
-                                + rs.getString("uf") + ", "
-                                + rs.getString("telefone") + ", "
-                                + rs.getString("documento") + ", "
-                                + rs.getString("email"));
-            }
+            int rowsAffected = st.executeUpdate();
+            
+            System.out.println("Feito! Linhas afetadas: " + rowsAffected);
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
         finally {
-            DB.closeResultSet(rs);
             DB.closeStatement(st);
             DB.closeConnection();
         }
-        
     }
     
 }
