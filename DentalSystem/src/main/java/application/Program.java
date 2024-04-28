821784157dea59f9383f7e3e7a68b4ea27fcb3e6
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-04-27 21:11:09 -0300

    criação objeto Cliente

--- DentalSystem/src/main/java/application/Program.java
+++ DentalSystem/src/main/java/application/Program.java
@@ -4,39 +4,13 @@
  */
 package application;
 
-import db.DB;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import model.entities.Cliente;
 
 public class Program {
     
     public static void main(String[] args) {
         
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DB.getConnection();
-            
-            st = conn.prepareStatement(
-                    "DELETE FROM cliente "
-                    + "WHERE "
-                    + "id_cliente = ?");
-            
-            st.setInt(1, 1);
-            
-            int linhasAfetadas = st.executeUpdate();
-            
-            System.out.println("Feito! Linhas afetadas: " + linhasAfetadas);
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+        Cliente c = new Cliente();
         
     }
-    
 }
