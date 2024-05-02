6c28be77db4647daa019ebae9aeb3b8d79e4e965
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-05-02 03:25:01 -0300

    criação buscarPorId

--- DentalSystem/src/main/java/model/controller/UsuarioController.java
+++ DentalSystem/src/main/java/model/controller/UsuarioController.java
@@ -1,5 +1,13 @@
 package model.controller;
 
+import java.sql.Connection;
+
 public class UsuarioController {
+    
+    private Connection conn;
+    
+    public UsuarioController(Connection conn) {
+        this.conn = conn;
+    }
     
 }
