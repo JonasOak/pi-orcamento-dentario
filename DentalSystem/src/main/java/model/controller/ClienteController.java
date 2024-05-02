2ab89c9275dec1c029d3050dc96d16c084d6b051
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-05-02 04:09:11 -0300

    implementação interface cliente

--- DentalSystem/src/main/java/model/controller/ClienteController.java
+++ DentalSystem/src/main/java/model/controller/ClienteController.java
@@ -6,17 +6,39 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import model.dao.ClienteDao;
 import model.entities.Cliente;
 
-public class ClienteController {
+public class ClienteController implements ClienteDao {
     
-    // Injeção de dependência para implementar conexão
     private Connection conn;
     
     public ClienteController(Connection conn) {
         this.conn = conn;
     }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void atualizar(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void deletarPorId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
+    @Override
+    public List<Cliente> buscarTodos() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
     public Cliente buscarPorId(Integer id) {
         PreparedStatement st = null;
         ResultSet rs = null;
@@ -28,14 +50,7 @@ public class ClienteController {
             st.setInt(1, id);
             rs = st.executeQuery();
             if (rs.next()) {
-                Cliente cliente = new Cliente();
-                cliente.setIdCliente(rs.getInt("id_cliente"));
-                cliente.setNomeCliente(rs.getString("nome"));
-                cliente.setEndereco(rs.getString("endereco"));
-                cliente.setUf(rs.getString("uf"));
-                cliente.setTelefone(rs.getString("telefone"));
-                cliente.setDocumento(rs.getString("documento"));
-                cliente.setEmail(rs.getString("email"));
+                Cliente cliente = instanciarCliente(rs);
                 return cliente;
             }
             return null;
@@ -48,5 +63,22 @@ public class ClienteController {
             BD.closeResultSet(rs);
         }
     }
+
+    @Override
+    public List<Cliente> buscarPorNome() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
+    // Método para instanciar cliente e não deixar o código verboso
+    private Cliente instanciarCliente(ResultSet rs) throws SQLException {
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(rs.getInt("id_cliente"));
+        cliente.setNomeCliente(rs.getString("nome"));
+        cliente.setEndereco(rs.getString("endereco"));
+        cliente.setUf(rs.getString("uf"));
+        cliente.setTelefone(rs.getString("telefone"));
+        cliente.setDocumento(rs.getString("documento"));
+        cliente.setEmail(rs.getString("email"));
+        return cliente;
+    }
 }
