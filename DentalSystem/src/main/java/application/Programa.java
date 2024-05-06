9097ff3bd7b68feada57d9917604463cfb2380d2
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-05-06 01:52:46 -0300

    testes

--- DentalSystem/src/main/java/application/Programa.java
+++ DentalSystem/src/main/java/application/Programa.java
@@ -1,26 +1,64 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import model.controller.ClienteController;
+import model.controller.OrcamentoController;
 import model.dao.DaoFactory;
 import model.entities.Cliente;
+import model.entities.Orcamento;
 
 public class Programa {
     
     public static void main(String[] args) {
         
         ClienteController clienteController = DaoFactory.criarClienteController();
-        List<Cliente> list;
+        OrcamentoController orcamentoController = DaoFactory.criarOrcamentoController();
+        List<Cliente> listC;
+        List<Orcamento> listO;
+        //Cliente cliente1 = new Cliente(4, "Maria", "Rua dAS fLORES, 321", "SP", "(11) 987654321", "12.345.678-90", "maria@gmail.com");
+        //Cliente cliente2 = new Cliente(5, "Alberto", "Rua Ferdinando, 123", "BA", "(71) 940028922", "12.431.493-21", "albertao@gmail.com");
+        //Orcamento orcamento = orcamentoController.buscarPorId(4);
+        //System.out.println(orcamento.toString());
         
         // Teste busca de cliente por id
         //Cliente cliente = clienteController.buscarPorId(2);
         //System.out.println(cliente);
         
-        // Teste buscar todos os clientes
-        list = clienteController.buscarTodos();
-        for (Cliente c : list) {
-            System.out.println(c);
+
+        // --------------------------------------------------------------------------------------------------------------
+        
+        // Teste inserir orçamento
+        /*
+        Orcamento[] orcamentos = {
+                new Orcamento(null, new Date(), "Planserv", new Date(), "Ela é boba", cliente1),
+                new Orcamento(null, new Date(), "Vitalmed", new Date(), "Ela é estranho", cliente2),
+                //new Orcamento(3, new Date(), "Plano 3", new Date(), "Observação 3", cliente1),
+        };
+
+        for (Orcamento o : orcamentos) {
+            orcamentoController.inserir(o);
+            System.out.println("Inserido! Novo id = " + o.getIdOrcamento());
         }
+        */
+        
+        // Teste deletar orçamento
+        //orcamentoController.deletarPorId(6);
+        //orcamentoController.deletarPorId(7);
+        
+        
+        //Orcamento orcamento = orcamentoController.buscarPorId(4);
+        //System.out.println(orcamento);
+        
+        
+        // Teste buscar por cliente
+        Cliente c = new Cliente(null, "ri", null, null, null, null, null);
+        List<Orcamento> list = orcamentoController.buscarPorCliente(c);
+        for (Orcamento obj : list) {
+            System.out.println(obj.toStringBuscaCliente());
+        }
+        
+        // --------------------------------------------------------------------------------------------------------------
         
         // Teste inserir cliente
         /*
@@ -43,14 +81,22 @@ public class Programa {
         
         // Teste atualizar cliente
         /*
-        Cliente cliente = clienteController.buscarPorId(3);
+        Cliente cliente = clienteController.buscarPorId(2);
+        System.out.println(cliente);
+        
         cliente.setEmail("girafola@gmail.com");
         clienteController.atualizar(cliente);
         */
         
         // Teste deletar cliente
         //clienteController.deletarPorId(3);
-
-
+        
+        // Teste buscar todos os clientes
+        
+//        listO = orcamentoController.buscarTodos();
+//        for (Orcamento o : listO) {
+//            System.out.println(o);
+//        }
+        
     }
 }
