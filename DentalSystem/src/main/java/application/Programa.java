cc9d32f58a94b82e9e9d166e1d58e33d34625a6f
Jonas Carvalho <jonasgabrielsc@gmail.com>
2024-05-25 18:51:50 -0300

    método main executavel

--- DentalSystem/src/main/java/application/Programa.java
+++ DentalSystem/src/main/java/application/Programa.java
@@ -5,7 +5,7 @@ import model.view.Menu;
 public class Programa {
     
     public static void main(String args[]) {
+        Menu.setNimbusLAF();
         java.awt.EventQueue.invokeLater(() -> new Menu().setVisible(true));
     }
-    
 }
--- DentalSystem/src/main/java/model/view/Menu.java
+++ DentalSystem/src/main/java/model/view/Menu.java
@@ -1,6 +1,8 @@
 package model.view;
 
 import javax.swing.JDesktopPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import model.entities.enums.Operacao;
 
 public class Menu extends javax.swing.JFrame {
@@ -160,40 +162,25 @@ public class Menu extends javax.swing.JFrame {
         tela.getBtConfirmar().setEnabled(false);
         tela.getTfOperacao().setText(operacao.name());
     }//GEN-LAST:event_abrirItemOrcamento
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+    
+    // Responsável por setar o LAF como Nimbus
+    public static void setNimbusLAF() {
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
+          for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+              UIManager.setLookAndFeel(info.getClassName());
+              break;
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+          ex.printStackTrace();
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Menu().setVisible(true);
-            }
-        });
+    }
+    
+    // Executável
+    public static void main(String args[]) {
+        setNimbusLAF();
+        java.awt.EventQueue.invokeLater(() -> new Menu().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
